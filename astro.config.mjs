38078fcd78a089a09d7239570cd8776e3731d208
Flamanis <Flamanis@users.noreply.github.com>
2025-06-07 04:20:29 -0500

    Try fastify

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ export default defineConfig({
   site: "https://nice-with-a-cup-of-tea-a60a19e5af38.herokuapp.com/",
   integrations: [preact()],
   adapter: node( {
-    mode: 'standalone',
+    mode: 'middleware',
   })
 });
